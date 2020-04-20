a7d6e86346cd4efb1f3aeb206c70c1ba49065d5f
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2020-04-20 13:54:57 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# GCC Contre Covid 19
+
+Génération des cartes de sensibilisation pour limiter la propagation du covid 19.
+
+🌟 Application web (PHP) 
+
+## Auteur
+
+**mmarcwabo**
+
+## Fonctionnalités
+
+* Ajouter votre nom, profession et photo
+* Generer votre carte de sensibilisation personnalisée
+* Télécharger votre carte de sensibilisation personalisée
+
+## Configurations
+* Dans le fichier config.php
--- index.php
+++ index.php
@@ -16,41 +16,46 @@ include_once './libs/config.php';
     <body style="background-color: #eee;">
         <div class="container">
             <div class="page-header" style="text-align: center; padding: 7px;">
-            <h1>GCC dit Stop Covid-19</h1>      
+                <h1>GCC dit Stop Covid-19</h1>      
             </div> 
             <div style="text-align: center; padding: 7px;">
-                <img src="img/logo-gcc.jpg" class="img-circle" alt="Goma Cycling Club" height="211" width="211">  
+                <img src="img/logo-gcc.jpg" class="img-circle" alt="Goma Cycling Club" height="169" width="169">  
             </div>         
-            <h2 class="title">Aidez-nous à sensibiliser pour limiter la propagation covid-19</h2>
+            <h3 class="" style="text-align: center; padding: 4px;">Aidez-nous à sensibiliser pour limiter la propagation du Corona virus et du Covid-19</h3>
             <?php if (isset($_SESSION['current_image'])) { ?>
-                
-                    <br/>
-                    <div class="form-group" style="text-align: center; padding: 7px;">
-                        <a href="<?php echo URL.$_SESSION['current_image']; ?>" download>
-                            <button class="btn btn-success">
-                                <i class="fa fa-download"></i> Télecharger ma carte de sensibilisation ici
-                            </button>
-                        </a>
-                    </div>
-                
+
+                <br/>
+                <div class="form-group" style="text-align: center; padding: 7px;">
+                    <a href="<?php echo URL . $_SESSION['current_image']; ?>" download>
+                        <button class="btn btn-success">
+                            <i class="fa fa-download"></i> Télecharger ma carte de sensibilisation ici
+                        </button>
+                    </a>
+                </div>
+
             <?php } ?>
             <form name="getGccCustom" action="image.php" method="POST" enctype="multipart/form-data">
                 <div id="message-to-download"></div>
                 <div class="form-group">
                     <label>Quel est votre nom ?</label>
-                    <input type="text" name="name" class="form-control" placeholder="Saisissez votre nom" />
+                    <input type="text" name="name" class="form-control" placeholder="Saisissez votre nom" required/>
                 </div>
                 <div class="form-group">
                     <label>Que faites-vous dans la vie ? </label>
-                    <input type="text" name="role" class="form-control" placeholder="Que faites-vous dans la vie?" />
+                    <input type="text" name="role" class="form-control" placeholder="Que faites-vous dans la vie?" required />
                 </div>
                 <div class="form-group">
                     <label>Ajouter votre photo : </label>
                     <input type="file" class="form-control" name="photo" />
                 </div>
-                <button type="submit" class="btn btn-success">Obtenir ma carte de sensibilisation</button>          
+                <button id="register" type="submit" class="btn btn-success">Obtenir ma carte de sensibilisation</button>          
             </form>
+                <br>
+        </div>
+        <div id="footer">
+            Made with ❤️ by <a href="https://exsofth.com">Exsofth Dev Team</a>
         </div>
         <script src="style/bootstrap.min.js"></script>
-    </body>
+        <script src="style/jquery.js"></script>
+        </body>
 </html>
