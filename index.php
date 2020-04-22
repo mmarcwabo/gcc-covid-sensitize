2e73d7635edca011eb3a997a5b49c7f1e67cbd25
Marcellin M. Wabo <5281242+mmarcwabo@users.noreply.github.com>
2020-04-22 03:14:49 +0100

    [NEW] Add cards counter and typed messages
    Add display of cards generated
    Show typed messages of habits to avoid
    covid sharing

--- index.php
+++ index.php
@@ -1,33 +1,41 @@
 <?php
 session_start();
 include_once './libs/config.php';
+include_once './libs/core_functions.php';
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>GCC Contre Covid-19</title>
+        <title><?= APP_NAME ?></title>
         <link rel="stylesheet" href="style/bootstrap.css">
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Add icon library -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
         <link rel="stylesheet" href="style/main.css">
     </head>
     <body style="background-color: #eee;">
         <div class="container">
             <div class="page-header" style="text-align: center; padding: 7px;">
-                <h1>GCC dit Stop Covid-19</h1>      
-            </div> 
+                <h3>GCC dit Stop Covid-19</h3>      
+            </div>
+
+            <div class="output" id="output" style="text-align: center; padding: 7px; font-weight: bold;">
+                <h5 class="cursor" style=" color: dodgerblue;"></h5>
+                <p style=" color: red;"></p>
+            </div>
+
             <div style="text-align: center; padding: 7px;">
                 <img src="img/logo-gcc.jpg" class="img-circle" alt="Goma Cycling Club" height="169" width="169">  
             </div>         
-            <h3 class="" style="text-align: center; padding: 4px;">Aidez-nous à sensibiliser pour limiter la propagation du Corona virus et du Covid-19</h3>
+            <h4 class="" style="text-align: center; padding: 4px;">Aidez-nous à sensibiliser pour limiter la propagation du Corona virus et du Covid-19</h4>
             <?php if (isset($_SESSION['current_image'])) { ?>
 
                 <br/>
                 <div class="form-group" style="text-align: center; padding: 7px;">
                     <a href="<?php echo URL . $_SESSION['current_image']; ?>" download>
-                        <button class="btn btn-success">
+                        <button class="btn btn-success" id="downloadB">
                             <i class="fa fa-download"></i> Télecharger ma carte de sensibilisation ici
                         </button>
                     </a>
@@ -42,7 +50,7 @@ include_once './libs/config.php';
                 </div>
                 <div class="form-group">
                     <label>Que faites-vous dans la vie ? </label>
-                    <input type="text" name="role" class="form-control" placeholder="Que faites-vous dans la vie?" required />
+                    <input type="text" name="role" class="form-control" placeholder="Avocat, Humanitaire, Médecin, Etudiant, etc" required />
                 </div>
                 <div class="form-group">
                     <label>Ajouter votre photo : </label>
@@ -50,12 +58,16 @@ include_once './libs/config.php';
                 </div>
                 <button id="register" type="submit" class="btn btn-success">Obtenir ma carte de sensibilisation</button>          
             </form>
-                <br>
+            <br>
         </div>
         <div id="footer">
+            <?= countGeneratedCards(); ?>
+            <span>-</span><br/>
+            <a href="https://covid19.devs-cast.com/" target="_blank">Voir la situation du covid19 en RDC</a><br/>
             Made with ❤️ by <a href="https://exsofth.com">Exsofth Dev Team</a>
+            
         </div>
         <script src="style/bootstrap.min.js"></script>
         <script src="style/jquery.js"></script>
-        </body>
-</html>
+        <script src="style/main.js"></script>
+    </body>
--- libs/core_functions.php
+++ libs/core_functions.php
@@ -22,13 +22,14 @@ function thumbnailAPic($file, $extension, $size_y, $size_x) {
         echo "Format de l'image non supporté";
         header('location: index.php?erreur');
     }
-    $destination = imagecreatetruecolor($size_y,$size_x);
+
+    $destination = imagecreatetruecolor($size_y, $size_x);
 // Les fonctions imagesx et imagesy renvoient la largeur et la
     $largeur_source = imagesx($source);
     $hauteur_source = imagesy($source);
     //$size_x = $size_y*($hauteur_source/$largeur_source);
     //Make it some proportional
-    
+
     $largeur_destination = imagesx($destination);
     $hauteur_destination = imagesy($destination);
 
@@ -37,6 +38,18 @@ function thumbnailAPic($file, $extension, $size_y, $size_x) {
             $largeur_destination, $hauteur_destination, $largeur_source,
             $hauteur_source);
 // On enregistre la miniature sous le nom "mini_couchersoleil.jpg"
-    $_SESSION["thumbnail_name"] = "img/thumbnails/".rand() . "_mini.png";
+    $_SESSION["thumbnail_name"] = "img/thumbnails/" . rand() . "_mini.png";
     imagepng($destination, $_SESSION["thumbnail_name"]);
+}
+
+function countGeneratedCards() {
+    $fi = new FilesystemIterator("img/thumbnails/", FilesystemIterator::SKIP_DOTS);
+    if(iterator_count($fi)==0){
+        return "Aucune carte generée <br/>";
+    }else if (iterator_count($fi)==1){
+        return iterator_count($fi). " Carte generée <br/>";
+    }else{
+         return iterator_count($fi). " Cartes generées <br/>";
+    }
+
 }
